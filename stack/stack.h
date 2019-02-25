b59d6c9f81fbb8947d66e8a9e9f13c2f913adf90
sousa <arthurricardo98@hotmail.com>
2019-02-25 17:17:50 -0300

    stack stuffs

---
+++ stack/stack.h
@@ -0,0 +1,39 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+typedef struct __node_ __node;  
+struct __node_ {
+                void *value;
+                __node *next; 
+            };
+
+typedef struct __stack __stack;
+struct __stack_ {
+                __node* top;
+                int size;
+            };
+            
+#define is_empty(stack) \
+        stack->top == NULL true:false
+        
+static inline void __new_node(__node *node, void *value) 
+{
+        node->next  = NULL;
+        node->value = value;
+}
+
+static inline void __new_stack(__stack *stack)
+{
+        stack->top  = NULL;
+        stack->size = 0;
+}            
+
+
+
+#ifndef /* STACK_H */
