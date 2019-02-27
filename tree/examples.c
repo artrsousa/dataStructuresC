789fe51b9d0d54e3d30180dd05a59f40223aab18
sousa <arthurricardo98@hotmail.com>
2019-02-27 10:46:14 -0300

    tree stuff

--- tree/examples.c
+++ tree/examples.c
@@ -4,5 +4,27 @@ int
 main(void) 
 {
         
+        {
+                
+                /*  examples of binary tree  */
+                __tree* tree = __new_tree();
+
+                __push(&(tree->root), (void*) 50);
+                __push(&(tree->root), (void*) 33);
+                __push(&(tree->root), (void*) 21);
+                __push(&(tree->root), (void*) 753);
+                __push(&(tree->root), (void*) 445);
+                __push(&(tree->root), (void*) 223);
+                __push(&(tree->root), (void*) 71);
+                __push(&(tree->root), (void*) 87);
+                        
+                __show(tree->root);
+
+                __search_for(tree->root, (void*) 55);
+
+                __clean_all(tree->root);
+
+        }
+        
         return 0;
 }
--- tree/tree.h
+++ tree/tree.h
@@ -17,9 +17,77 @@ struct __node_ {
 typedef struct __tree_ __tree;
 struct __tree_ {
                 __node* root;
-                int size;
         };
         
+static inline void __clean_all(__node *root)
+{
+        if (root != NULL)
+        {        
+                __clean_all(root->left);
+                __clean_all(root->right);
+                free(root);
+        }
+        
+}
+                
+static inline __node *__new_node(void *value)
+{
+        __node *node = (__node*) malloc(sizeof(__node));
+        node->left  = node->right = NULL;
+        node->value = (void*) malloc(sizeof(*value));
+        node->value = value;
+        
+        return node;
+}
 
+static inline __tree *__new_tree(void)
+{
+        __tree *tree = malloc(sizeof(__tree));
+        tree->root   = NULL;
+        
+        return tree;
+}
+
+static inline void __push(__node **node, void *value)
+{
+        if (*node == NULL) 
+        { 
+                *node = __new_node(value);
+        }
+        else if (value < (*node)->value)
+        {
+                __push(&(*node)->left, value);
+        }
+        else if (value >= (*node)->value)
+        {
+                __push(&(*node)->right, value);
+        }
+}
+
+static inline void __show(__node *node)
+{
+        if (node != NULL) {
+                __show(node->left);
+                printf("node value: %d\n", node->value);
+                __show(node->right);
+        }
+}
+
+static inline void __search_for(__node *root, void *value)
+{
+        if (root != NULL) 
+        {
+                if (root->value == value) {
+                        printf("[%d] FOUND!! \n", value);
+                }
+                else 
+                {
+                        if (value < root->value)
+                                __search_for(root->left, value);
+                        else if (value > root->value)
+                                __search_for(root->right, value);
+                }
+        }
+}
 
 #endif /*TREE_H*/
